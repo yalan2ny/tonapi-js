3a348de65a316ab121d04c7d570585acaea1427d
Moiseev Ilya <ilya@mois.pro>
2025-03-16 02:32:23 +0400

    test: memory leak

--- packages/client/tests/__mock__/services.ts
+++ packages/client/tests/__mock__/services.ts
@@ -168,3 +168,254 @@ export const getRates = JSONStringify({
         }
     }
 });
+
+export const getBlockchainBlockTransactions = JSONStringify({
+    transactions: [
+        {
+            hash: 'c42b349710ad6d30a1a63a06875439ec17a6ac1710f098c8be64d1397c9d9b62',
+            lt: 6826225000001,
+            account: {
+                address: '-1:3333333333333333333333333333333333333333333333333333333333333333',
+                name: 'Elector Contract',
+                is_scam: false,
+                is_wallet: false
+            },
+            success: true,
+            utime: 1591474123,
+            orig_status: 'active',
+            end_status: 'active',
+            total_fees: 0,
+            end_balance: 20104154781099636,
+            transaction_type: 'TransTickTock',
+            state_update_old: 'd560d4f8e139d819e7a07a6150c3e8fec8e339b8d860148c1c4d5a538b6f06e4',
+            state_update_new: '18137f0ec62e48da5a5025c69004b64c127cbe1b90d7f4cdb63a315f62220e00',
+            out_msgs: [],
+            block: '(-1,8000000000000000,4234234)',
+            prev_trans_hash: 'f6510b12bf312564dedfaa26b1fe98f99a920dd24817d8b6e03a30c4e61d4fed',
+            prev_trans_lt: 6826223000002,
+            compute_phase: {
+                skipped: false,
+                success: true,
+                gas_fees: 0,
+                gas_used: 5781,
+                vm_steps: 136,
+                exit_code: 0,
+                exit_code_description: 'Ok'
+            },
+            action_phase: {
+                success: true,
+                result_code: 0,
+                total_actions: 0,
+                skipped_actions: 0,
+                fwd_fees: 0,
+                total_fees: 0
+            },
+            aborted: false,
+            destroyed: false,
+            raw: 'b5ee9c720102060100012b0003af73333333333333333333333333333333333333333333333333333333333333333000006355ab75e41f6510b12bf312564dedfaa26b1fe98f99a920dd24817d8b6e03a30c4e61d4fed000006355a98d9c25edbf7cb0001408010203000120008272d560d4f8e139d819e7a07a6150c3e8fec8e339b8d860148c1c4d5a538b6f06e418137f0ec62e48da5a5025c69004b64c127cbe1b90d7f4cdb63a315f62220e0002052030240405009e42d2ae625a0000000000000000008800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005bc00000000000000000000000012d452da449e50b8cf7dd27861f146122afe1b546bb8b70fc8216f0c614139f8e04'
+        },
+        {
+            hash: '1d05e35e9f0968fe1ab3d7844ff1741beea8bdc93f180c113db627d8aa667468',
+            lt: 6826225000001,
+            account: {
+                address: '-1:34517c7bdf5187c55af4f8b61fdc321588c7ab768dee24b006df29106458d7cf',
+                name: 'Log tests Contract',
+                is_scam: false,
+                is_wallet: false
+            },
+            success: false,
+            utime: 1591474123,
+            orig_status: 'active',
+            end_status: 'active',
+            total_fees: 0,
+            end_balance: 196000000,
+            transaction_type: 'TransTickTock',
+            state_update_old: 'fb6aa671d8268afdbfa297c0477593f7f79ab20bdc6520cc9253bb7920e7a0bf',
+            state_update_new: '9ded6399fb6df92fa99ffd87f5175a5c97f852236fee6ccaf5eb4643a9c25c3d',
+            out_msgs: [],
+            block: '(-1,8000000000000000,4234234)',
+            prev_trans_hash: 'eeb9ccfb5e7cb9e56fa914f3b8f76ce78fa340cca0c79bd49e4c942a2e2d6af0',
+            prev_trans_lt: 6826223000003,
+            compute_phase: {
+                skipped: false,
+                success: true,
+                gas_fees: 0,
+                gas_used: 4914,
+                vm_steps: 48,
+                exit_code: 0,
+                exit_code_description: 'Ok'
+            },
+            action_phase: {
+                success: false,
+                result_code: 37,
+                total_actions: 3,
+                skipped_actions: 0,
+                fwd_fees: 0,
+                total_fees: 0,
+                result_code_description: 'Insufficient TON'
+            },
+            aborted: true,
+            destroyed: false,
+            raw: 'b5ee9c72010206010001320003af734517c7bdf5187c55af4f8b61fdc321588c7ab768dee24b006df29106458d7cf000006355ab75e41eeb9ccfb5e7cb9e56fa914f3b8f76ce78fa340cca0c79bd49e4c942a2e2d6af0000006355a98d9c35edbf7cb0001408010203000120008272fb6aa671d8268afdbfa297c0477593f7f79ab20bdc6520cc9253bb7920e7a0bf9ded6399fb6df92fa99ffd87f5175a5c97f852236fee6ccaf5eb4643a9c25c3d02052030340405009e42664e625a00000000000000000030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000069600000009600000004000600000000000519ae84f17b8f8b22026a975ff55f1ab19fde4a768744d2178dfa63bb533e107a409026bc'
+        },
+        {
+            hash: 'fef6c155797aedd6c5406f8e85a279d8030645beadd2af3a4cb29219242f94f5',
+            lt: 6826225000002,
+            account: {
+                address: '-1:3333333333333333333333333333333333333333333333333333333333333333',
+                name: 'Elector Contract',
+                is_scam: false,
+                is_wallet: false
+            },
+            success: true,
+            utime: 1591474123,
+            orig_status: 'active',
+            end_status: 'active',
+            total_fees: 0,
+            end_balance: 20104157856099636,
+            transaction_type: 'TransOrd',
+            state_update_old: '18137f0ec62e48da5a5025c69004b64c127cbe1b90d7f4cdb63a315f62220e00',
+            state_update_new: '7f69319fbaad5835dfa21dab345af1898fe73214bd46f5bfe3e8fed959b1bd14',
+            in_msg: {
+                msg_type: 'int_msg',
+                created_lt: 6826225000000,
+                ihr_disabled: true,
+                bounce: true,
+                bounced: false,
+                value: 3075000000,
+                fwd_fee: 0,
+                ihr_fee: 0,
+                destination: {
+                    address: '-1:3333333333333333333333333333333333333333333333333333333333333333',
+                    name: 'Elector Contract',
+                    is_scam: false,
+                    is_wallet: false
+                },
+                source: {
+                    address: '-1:0000000000000000000000000000000000000000000000000000000000000000',
+                    name: 'System',
+                    is_scam: false,
+                    is_wallet: false
+                },
+                import_fee: 0,
+                created_at: 1591474123,
+                hash: '8368aa2f599e21a062e7e48b53e4ff1a0249cdd48c96e61b974b27992678c0d6'
+            },
+            out_msgs: [],
+            block: '(-1,8000000000000000,4234234)',
+            prev_trans_hash: 'c42b349710ad6d30a1a63a06875439ec17a6ac1710f098c8be64d1397c9d9b62',
+            prev_trans_lt: 6826225000001,
+            compute_phase: {
+                skipped: false,
+                success: true,
+                gas_fees: 0,
+                gas_used: 5499,
+                vm_steps: 100,
+                exit_code: 0,
+                exit_code_description: 'Ok'
+            },
+            credit_phase: {
+                fees_collected: 0,
+                credit: 3075000000
+            },
+            action_phase: {
+                success: true,
+                result_code: 0,
+                total_actions: 0,
+                skipped_actions: 0,
+                fwd_fees: 0,
+                total_fees: 0
+            },
+            aborted: false,
+            destroyed: false,
+            raw: 'b5ee9c72010207010001890003af73333333333333333333333333333333333333333333333333333333333333333000006355ab75e42c42b349710ad6d30a1a63a06875439ec17a6ac1710f098c8be64d1397c9d9b62000006355ab75e415edbf7cb00014080102030101a00400827218137f0ec62e48da5a5025c69004b64c127cbe1b90d7f4cdb63a315f62220e007f69319fbaad5835dfa21dab345af1898fe73214bd46f5bfe3e8fed959b1bd14020f04092dd231b01811050600ab69fe00000000000000000000000000000000000000000000000000000000000000013fccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccd2dd231b000000000c6ab56ebc80bdb7ef9640009e42af6c12c4b000000000000000006400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005bc00000000000000000000000012d452da449e50b8cf7dd27861f146122afe1b546bb8b70fc8216f0c614139f8e04'
+        },
+        {
+            hash: '2f2d0e7f7f2c920f0994be6dd711c7b0822d2525ec2083acc1d9136d48a7a7ef',
+            lt: 6826225000003,
+            account: {
+                address: '-1:5555555555555555555555555555555555555555555555555555555555555555',
+                name: 'Config Contract',
+                is_scam: false,
+                is_wallet: false
+            },
+            success: true,
+            utime: 1591474123,
+            orig_status: 'active',
+            end_status: 'active',
+            total_fees: 0,
+            end_balance: 57614730896,
+            transaction_type: 'TransTickTock',
+            state_update_old: 'dddc3c035d0976f83850b5ade7b8c14ddd9949d84459e70eb5bf041333db8a06',
+            state_update_new: '3ecc4223da9b93096b998e3136cd7f603d8547db42ec08649cb0c2bdda09f732',
+            out_msgs: [],
+            block: '(-1,8000000000000000,4234234)',
+            prev_trans_hash: 'd1ddfbf892c165fb71b987be6de67ffd4415da176e2b282ecad1888042c2d45c',
+            prev_trans_lt: 6826223000003,
+            compute_phase: {
+                skipped: false,
+                success: true,
+                gas_fees: 0,
+                gas_used: 2785,
+                vm_steps: 66,
+                exit_code: 0,
+                exit_code_description: 'Ok'
+            },
+            action_phase: {
+                success: true,
+                result_code: 0,
+                total_actions: 0,
+                skipped_actions: 0,
+                fwd_fees: 0,
+                total_fees: 0
+            },
+            aborted: false,
+            destroyed: false,
+            raw: 'b5ee9c720102060100012b0003af75555555555555555555555555555555555555555555555555555555555555555000006355ab75e43d1ddfbf892c165fb71b987be6de67ffd4415da176e2b282ecad1888042c2d45c000006355a98d9c35edbf7cb0001408010203000120008272dddc3c035d0976f83850b5ade7b8c14ddd9949d84459e70eb5bf041333db8a063ecc4223da9b93096b998e3136cd7f603d8547db42ec08649cb0c2bdda09f73202053030240405009e415c2e625a0000000000000000004200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005bc00000000000000000000000012d452da449e50b8cf7dd27861f146122afe1b546bb8b70fc8216f0c614139f8e04'
+        },
+        {
+            hash: 'fe659d5da8d50422f8a0aa88caa9ad066db4eb508ffe393c9105a4617d068fee',
+            lt: 6826225000003,
+            account: {
+                address: '-1:34517c7bdf5187c55af4f8b61fdc321588c7ab768dee24b006df29106458d7cf',
+                name: 'Log tests Contract',
+                is_scam: false,
+                is_wallet: false
+            },
+            success: false,
+            utime: 1591474123,
+            orig_status: 'active',
+            end_status: 'active',
+            total_fees: 0,
+            end_balance: 196000000,
+            transaction_type: 'TransTickTock',
+            state_update_old: '9ded6399fb6df92fa99ffd87f5175a5c97f852236fee6ccaf5eb4643a9c25c3d',
+            state_update_new: '2ad7b335d348d711b1d5397b626bdd3bcb3e808fd9ac9f32c4ca3a242babaa59',
+            out_msgs: [],
+            block: '(-1,8000000000000000,4234234)',
+            prev_trans_hash: '1d05e35e9f0968fe1ab3d7844ff1741beea8bdc93f180c113db627d8aa667468',
+            prev_trans_lt: 6826225000001,
+            compute_phase: {
+                skipped: false,
+                success: true,
+                gas_fees: 0,
+                gas_used: 4914,
+                vm_steps: 48,
+                exit_code: 0,
+                exit_code_description: 'Ok'
+            },
+            action_phase: {
+                success: false,
+                result_code: 37,
+                total_actions: 3,
+                skipped_actions: 0,
+                fwd_fees: 0,
+                total_fees: 0,
+                result_code_description: 'Insufficient TON'
+            },
+            aborted: true,
+            destroyed: false,
+            raw: 'b5ee9c72010206010001320003af734517c7bdf5187c55af4f8b61fdc321588c7ab768dee24b006df29106458d7cf000006355ab75e431d05e35e9f0968fe1ab3d7844ff1741beea8bdc93f180c113db627d8aa667468000006355ab75e415edbf7cb00014080102030001200082729ded6399fb6df92fa99ffd87f5175a5c97f852236fee6ccaf5eb4643a9c25c3d2ad7b335d348d711b1d5397b626bdd3bcb3e808fd9ac9f32c4ca3a242babaa5902053030340405009e42664e625a00000000000000000030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000069600000009600000004000600000000000519ae84f17b8f8b22026a975ff55f1ab19fde4a768744d2178dfa63bb533e107a409026bc'
+        }
+    ]
+});
