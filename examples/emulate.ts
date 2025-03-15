eda4a8ed3acd0d9edf1a557e800c88ccfb0872e6
Moiseev Ilya <ilya@mois.pro>
2025-03-16 00:46:02 +0400

    chore: update emulate & gasless examples

--- examples/emulate.ts
+++ examples/emulate.ts
@@ -1,78 +1,81 @@
-import { WalletContractV4 } from '@ton/ton';
-import { Address, beginCell, internal, toNano, SendMode, external, storeMessage } from '@ton/core';
+import {
+    WalletContractV4,
+    Address,
+    beginCell,
+    internal,
+    toNano,
+    SendMode,
+    external,
+    storeMessage
+} from '@ton/ton';
 import { mnemonicNew, mnemonicToPrivateKey } from '@ton/crypto';
 import { TonApiClient } from '@ton-api/client';
 
-// if you need to send lots of requests in parallel,
-// make sure you use a tonapi token.
+// if you need to send lots of requests in parallel, make sure you use a tonapi token.
 const ta = new TonApiClient({
-    baseUrl: 'https://tonapi.io'
-    // apiKey: 'YOUR_API_KEY',
+    baseUrl: 'https://tonapi.io',
+    apiKey: 'YOUR_API_KEY'
 });
 
+// Sender's wallet address
+const senderAddress = Address.parse('UQAQxxpzxmEVU0Lu8U0zNTxBzXIWPvo263TIN1OQM9YvxsnV');
+const recipientAddress = Address.parse('UQDNzlh0XSZdb5_Qrlx5QjyZHVAO74v5oMeVVrtF_5Vt1rIt');
+
+// Get wallet's seqno and public key
+const { seqno } = await ta.wallet.getAccountSeqno(senderAddress);
+const { publicKey: publicKeyHex } = await ta.accounts.getAccountPublicKey(senderAddress);
+
 // Emulate transaction from wallet_v4 address
-const emulateTransaction = async () => {
-    // Sender's wallet address
-    const senderAddress = Address.parse('UQAQxxpzxmEVU0Lu8U0zNTxBzXIWPvo263TIN1OQM9YvxsnV');
-    const recipientAddress = Address.parse('UQDNzlh0XSZdb5_Qrlx5QjyZHVAO74v5oMeVVrtF_5Vt1rIt');
+const wallet = WalletContractV4.create({
+    workchain: 0,
+    publicKey: Buffer.from(publicKeyHex, 'hex')
+});
 
-    // Get wallet's seqno and public key
-    const { seqno } = await ta.wallet.getAccountSeqno(senderAddress);
-    const { publicKey: publicKeyHex } = await ta.accounts.getAccountPublicKey(senderAddress);
+// Create dummy private key
+const dummyKey = (await mnemonicToPrivateKey(await mnemonicNew())).secretKey;
 
-    const wallet = WalletContractV4.create({
-        workchain: 0,
-        publicKey: Buffer.from(publicKeyHex, 'hex')
-    });
+// Generate payload for NFT transfer
+const body = beginCell()
+    .storeUint(0x5fcc3d14, 32) // Operation code for NFT transfer
+    .storeUint(0, 64) // Query ID
+    .storeAddress(recipientAddress) // Recipient address
+    .storeAddress(senderAddress) // Sender address
+    .storeUint(0, 1)
+    .storeCoins(toNano('0.0000001')) // Small transfer fee
+    .storeBit(0) // No custom payload
+    .endCell();
 
-    // Create dummy private key
-    const dummyKey = (await mnemonicToPrivateKey(await mnemonicNew())).secretKey;
+// Create transfer for emulation
+const transferCell = wallet.createTransfer({
+    seqno,
+    secretKey: dummyKey,
+    sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
+    messages: [
+        internal({
+            value: toNano('0.5'),
+            to: Address.parse('EQDvK4AbmarjScYfcfF95XLX5y1ges3zPWgOZavXo0SMmqH9'),
+            body: body
+        })
+    ]
+});
 
-    // Generate payload for NFT transfer
-    const body = beginCell()
-        .storeUint(0x5fcc3d14, 32) // Operation code for NFT transfer
-        .storeUint(0, 64) // Query ID
-        .storeAddress(recipientAddress) // Recipient address
-        .storeAddress(senderAddress) // Sender address
-        .storeUint(0, 1)
-        .storeCoins(toNano('0.0000001')) // Small transfer fee
-        .storeBit(0) // No custom payload
-        .endCell();
-
-    // Create transfer for emulation
-    const tr = wallet.createTransfer({
-        seqno,
-        secretKey: dummyKey,
-        sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
-        messages: [
-            internal({
-                value: toNano('0.5'),
-                to: Address.parse('EQDvK4AbmarjScYfcfF95XLX5y1ges3zPWgOZavXo0SMmqH9'),
-                body: body
+// Create external message for emulation
+const bocExternalMessage = beginCell()
+    .store(
+        storeMessage(
+            external({
+                to: senderAddress,
+                init: undefined,
+                body: transferCell
             })
-        ]
-    });
-
-    // Create external message for emulation
-    const bocExternalMessage = beginCell()
-        .store(
-            storeMessage(
-                external({
-                    to: senderAddress,
-                    init: undefined,
-                    body: tr
-                })
-            )
         )
-        .endCell();
+    )
+    .endCell();
 
-    // Emulate transaction
-    const emulateTrace = await ta.emulation.emulateMessageToTrace(
-        { boc: bocExternalMessage },
-        { ignore_signature_check: true } // Ignore signature for execute message from other account
-    );
+// Emulate transaction
+const emulateTrace = await ta.emulation.emulateMessageToTrace(
+    { boc: bocExternalMessage },
+    { ignore_signature_check: true } // Ignore signature for execute message from other account
+);
 
-    console.log(emulateTrace);
-};
-
-emulateTransaction();
+console.log(emulateTrace);
--- examples/gasless.ts
+++ examples/gasless.ts
@@ -1,5 +1,6 @@
-import { WalletContractV5R1 } from '@ton/ton';
+import { mnemonicToPrivateKey } from '@ton/crypto';
 import {
+    WalletContractV5R1,
     Address,
     beginCell,
     internal,
@@ -8,17 +9,15 @@ import {
     external,
     storeMessage,
     storeMessageRelaxed
-} from '@ton/core';
-import { mnemonicToPrivateKey } from '@ton/crypto';
+} from '@ton/ton';
 
 import { TonApiClient } from '@ton-api/client';
 import { ContractAdapter } from '@ton-api/ton-adapter';
 
-// if you need to send lots of requests in parallel,
-// make sure you use a tonapi token.
+// if you need to send lots of requests in parallel, make sure you use a tonapi token.
 const ta = new TonApiClient({
-    baseUrl: 'https://tonapi.io'
-    // apiKey: 'YOUR_API_KEY',
+    baseUrl: 'https://tonapi.io',
+    apiKey: 'YOUR_API_KEY'
 });
 
 const provider = new ContractAdapter(ta);
@@ -31,119 +30,112 @@ const OP_CODES = {
 // Amount for jetton transfer. Usually 0.05 TON is enough for most jetton transfers without forwardBody
 const BASE_JETTON_SEND_AMOUNT = toNano(0.05);
 
-const main = async () => {
-    
-    // this is a simple example of how to send a gasless transfer.
-    // you only need to specify your seed and a destination address.
+// this is a simple example of how to send a gasless transfer.
+// you only need to specify your seed and a destination address.
 
-    // the seed is not sent to the network, it is used to sign messages locally.
+// the seed is not sent to the network, it is used to sign messages locally.
 
-    const seed = '..!!! REPLACE THIS WITH YOUR SEED !!! ..'; // wallet seed `word1 word2 word3 ... word24`
-    const destination = Address.parse('..!!! REPLACE THIS WITH A CORRECT DESTINATION !!!..'); // replace with a correct recipient address
-    const usdtMaster = Address.parse('EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs'); // USDt jetton master.
+const mnemonics = 'word1 word2 ...'.split(' '); // wallet seed `word1 word2 word3 ... word24`
+const destination = Address.parse('UQApwowlR6X54bXoso6orKCzCNm9ily8pAFy5vTwmsQ2WvVi'); // replace with a correct recipient address
+const usdtMaster = Address.parse('EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs'); // USDt jetton master.
 
-    const jettonAmount = 1_000_000n; // amount in the smallest jetton units. This is 1 USDt.
+const jettonAmount = 1_000_000n; // amount in the smallest jetton units. This is 1 USDt.
 
-    const keyPair = await mnemonicToPrivateKey(seed.split(' '));
-    const workchain = 0;
-    const wallet = WalletContractV5R1.create({
-        workchain,
-        publicKey: keyPair.publicKey
-    });
-    const contract = provider.open(wallet);
+const keyPair = await mnemonicToPrivateKey(mnemonics);
+const wallet = WalletContractV5R1.create({ publicKey: keyPair.publicKey });
+const contract = provider.open(wallet);
 
-    console.log('Wallet address:', wallet.address.toString());
+console.log('Wallet address:', wallet.address.toString());
 
-    const jettonWalletAddressResult = await ta.blockchain.execGetMethodForBlockchainAccount(
-        usdtMaster,
-        'get_wallet_address',
-        {
-            args: [wallet.address.toRawString()]
-        }
-    );
+const jettonWalletAddressResult = await ta.blockchain.execGetMethodForBlockchainAccount(
+    usdtMaster,
+    'get_wallet_address',
+    {
+        args: [wallet.address.toRawString()]
+    }
+);
 
-    const jettonWallet = Address.parse(jettonWalletAddressResult.decoded.jettonWalletAddress);
+const jettonWallet = Address.parse(jettonWalletAddressResult.decoded.jetton_wallet_address);
 
-    // we use USDt in this example,
-    // so we just print all supported gas jettons and get the relay address.
-    // we have to send excess to the relay address in order to make a transfer cheaper.
-    const relayerAddress = await printConfigAndReturnRelayAddress();
+// we use USDt in this example,
+// so we just print all supported gas jettons and get the relay address.
+// we have to send excess to the relay address in order to make a transfer cheaper.
+const relayerAddress = await printConfigAndReturnRelayAddress();
 
-    // Create payload for jetton transfer
-    const tetherTransferPayload = beginCell()
-        .storeUint(OP_CODES.JETTON_TRANSFER, 32)
-        .storeUint(0, 64)
-        .storeCoins(jettonAmount) // 1 USDT
-        .storeAddress(destination) // address for receiver
-        .storeAddress(relayerAddress) // address for excesses
-        .storeBit(false) // null custom_payload
-        .storeCoins(1n) // count of forward transfers in nanoton
-        .storeMaybeRef(undefined)
-        .endCell();
+// Create payload for jetton transfer
+const tetherTransferPayload = beginCell()
+    .storeUint(OP_CODES.JETTON_TRANSFER, 32)
+    .storeUint(0, 64)
+    .storeCoins(jettonAmount) // 1 USDT
+    .storeAddress(destination) // address for receiver
+    .storeAddress(relayerAddress) // address for excesses
+    .storeBit(false) // null custom_payload
+    .storeCoins(1n) // count of forward transfers in nanoton
+    .storeMaybeRef(undefined)
+    .endCell();
 
-    const messageToEstimate = beginCell()
-        .storeWritable(
-            storeMessageRelaxed(
-                internal({
-                    to: jettonWallet,
-                    bounce: true,
-                    value: BASE_JETTON_SEND_AMOUNT,
-                    body: tetherTransferPayload
-                })
-            )
-        )
-        .endCell();
-
-    // we send a single message containing a transfer from our wallet to a desired destination.
-    // as a result of estimation, TonAPI returns a list of messages that we need to sign.
-    // the first message is a fee transfer to the relay address, the second message is our original transfer.
-    const params = await ta.gasless.gaslessEstimate(usdtMaster, {
-        walletAddress: wallet.address,
-        walletPublicKey: keyPair.publicKey.toString('hex'),
-        messages: [{ boc: messageToEstimate }]
-    }); //.catch(error => console.error(error));
-
-    console.log('Estimated transfer:', params);
-
-    const seqno = await contract.getSeqno();
-
-    // params is the same structure as params in tonconnect
-    const tetherTransferForSend = wallet.createTransfer({
-        seqno,
-        authType: 'internal',
-        timeout: Math.ceil(Date.now() / 1000) + 5 * 60,
-        secretKey: keyPair.secretKey,
-        sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
-        messages: params.messages.map(message =>
+const messageToEstimate = beginCell()
+    .storeWritable(
+        storeMessageRelaxed(
             internal({
-                to: message.address,
-                value: BigInt(message.amount),
-                body: message.payload
+                to: jettonWallet,
+                bounce: true,
+                value: BASE_JETTON_SEND_AMOUNT,
+                body: tetherTransferPayload
             })
         )
-    });
+    )
+    .endCell();
 
-    const extMessage = beginCell()
-        .storeWritable(
-            storeMessage(
-                external({
-                    to: contract.address,
-                    init: seqno === 0 ? contract.init : undefined,
-                    body: tetherTransferForSend
-                })
-            )
-        )
-        .endCell();
+// we send a single message containing a transfer from our wallet to a desired destination.
+// as a result of estimation, TonAPI returns a list of messages that we need to sign.
+// the first message is a fee transfer to the relay address, the second message is our original transfer.
+const params = await ta.gasless.gaslessEstimate(usdtMaster, {
+    walletAddress: wallet.address,
+    walletPublicKey: keyPair.publicKey.toString('hex'),
+    messages: [{ boc: messageToEstimate }]
+}); //.catch(error => console.error(error));
 
-    // Send a gasless transfer
-    ta.gasless
-        .gaslessSend({
-            walletPublicKey: keyPair.publicKey.toString('hex'),
-            boc: extMessage
+console.log('Estimated transfer:', params);
+
+const seqno = await contract.getSeqno();
+
+// params is the same structure as params in tonconnect
+const tetherTransferForSend = wallet.createTransfer({
+    seqno,
+    authType: 'internal',
+    timeout: Math.ceil(Date.now() / 1000) + 5 * 60,
+    secretKey: keyPair.secretKey,
+    sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
+    messages: params.messages.map(message =>
+        internal({
+            to: message.address,
+            value: BigInt(message.amount),
+            body: message.payload
         })
-        .then(() => console.log('A gasless transfer sent!'))
-        .catch(error => console.error(error.message));
-};
+    )
+});
+
+const extMessage = beginCell()
+    .storeWritable(
+        storeMessage(
+            external({
+                to: contract.address,
+                init: seqno === 0 ? contract.init : undefined,
+                body: tetherTransferForSend
+            })
+        )
+    )
+    .endCell();
+
+// Send a gasless transfer
+ta.gasless
+    .gaslessSend({
+        walletPublicKey: keyPair.publicKey.toString('hex'),
+        boc: extMessage
+    })
+    .then(() => console.log('A gasless transfer sent!'))
+    .catch(error => console.error(error.message));
 
 async function printConfigAndReturnRelayAddress(): Promise<Address> {
     const cfg = await ta.gasless.gaslessConfig();
@@ -154,5 +146,3 @@ async function printConfigAndReturnRelayAddress(): Promise<Address> {
     console.log(`Relay address to send fees to: ${cfg.relayAddress}`);
     return cfg.relayAddress;
 }
-
-main().catch(console.error);
